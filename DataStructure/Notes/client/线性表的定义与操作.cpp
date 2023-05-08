e8361015ffa2f7b2070768ab886affc7f053c376
3000ye <hcg2924239214@163.com>
2023-05-08 11:54:40 +0800

    线性表

---
+++ DataStructure/Notes/client/线性表的定义与操作.cpp
@@ -0,0 +1,17 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+typedef int Postion;
+typedef struct LNode *List;
+
+struct LNode {
+    ElementType Data[MAXSIZE];
+    Postion Last;
+}
+
+int main() {
+
+
+    return 0;
+}
