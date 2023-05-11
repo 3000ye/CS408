87cdf0766ed0afe6e464338fc398c6cb96e5161f
3000ye <hcg2924239214@163.com>
2023-05-11 23:21:31 +0800

    静态链表

---
+++ DataStructure/Notes/client/静态链表.cpp
@@ -0,0 +1,16 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define MaxSize 10
+typedef struct {
+    int val;
+    int next;
+} SListLink[MaxSize];
+
+int main() {
+    SListLink ls;
+
+
+    return 0;
+}
